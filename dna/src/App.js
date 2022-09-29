1818d47a7169bdf0f06dfee9c6fa3e532b175a7b
priyanka <priyankaphulmante98@gmail.com>
2022-09-29 16:08:47 +0530

    day-3

---
+++ dna/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar />
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ dna/src/data/category.js
@@ -0,0 +1,18 @@
+const categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+    "World",
+    "photo",
+    "latest News",
+    "Lifestyle",
+    "viral News",
+    "Ezmall"
+
+  ];
+  export default categories;
+  
