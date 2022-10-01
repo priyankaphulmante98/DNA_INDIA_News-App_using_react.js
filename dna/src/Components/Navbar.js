261fde2c28ad54bd9c6912ed55cf7317ec5a53d1
priyanka <priyankaphulmante98@gmail.com>
2022-10-01 17:11:10 +0530

    day-5

--- dna/src/Components/Navbar.js
+++ dna/src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.css';
 import HamburgerDrawer from './HamburgerDrawer'
 
+
 const Navbar = (setCategory) => {
   return (
     <div>
@@ -16,12 +17,49 @@ const Navbar = (setCategory) => {
        src="https://english.cdn.zeenews.com/static/apprun/dna/icons/logodna.png" 
        height='60%'
        alt='logo'/>
-
+      
+      
       <button className="hindi" href="">हिंदी में पढ़ें</button>
+      </div>
+      <div className="tired">
+        <img
+      style={{ cursor:"pointer" , height:"500px", width:"900px" }}
+       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGwx5kGKPrWY9L_7GAHlMiA31DmBnAUiGuAw&usqp=CAU" 
+       alt='logo'/>
+      </div>
+      
+      <div className="tired">
+        <img
+      style={{ cursor:"pointer" , height:"500px", width:"900px" }}
+       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQZc_JiYB-txhStVqjPa_u1EBEdo7ZsMzA4g&usqp=CAU" 
+       alt='logo'/>
+      </div>
+      <div className="tired">
+        <img
+      style={{ cursor:"pointer" , height:"500px", width:"900px" }}
+       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPml-BFNpkMZau4WOxuxK9xQthceFPc7MoNw&usqp=CAU" 
+       alt='logo'/>
+      </div>
 
-       </div>
+      <div className="tired">
+        <img
+      style={{ cursor:"pointer" , height:"500px", width:"900px" }}
+       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSluMMGMlGH3ALdH67i7mVEWrpRSFjBOE0xpw&usqp=CAU" 
+       alt='logo'/>
+      </div>
+      
+      <div className="tired">
+        <img
+      style={{ cursor:"pointer" , height:"500px", width:"900px" }}
+       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy0eXIEFymGf2pWogRNDXBkSLz6g3zidMvXY9JdfE1hiO2C9d97AQjwFmusSS2cFH2ba0&usqp=CAU" 
+       alt='logo'/>
+      </div>
+
        </div>
    
+  
+  
+ 
   );
 }
 
